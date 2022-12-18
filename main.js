b7cc7e5d196191821c5d40cac65a41454413ddb6
Nicolas Gnyra <nicolas@gnyra.com>
2022-12-18 16:34:45 -0500

    Update packages & bump Node version

--- main.js
+++ main.js
@@ -1,34 +1,34 @@
-const core = require('@actions/core');
-const fetch = require('node-fetch');
-const semver = require('semver');
-const unzip = require('unzipper');
-const fs = require("fs");
+import { info, getInput, warning, setFailed } from '@actions/core';
+import fetch from 'node-fetch';
+import { satisfies } from 'semver';
+import { Extract } from 'unzipper';
+import { readFileSync } from "fs";
 
-main().then(() => core.info("Complete!"))
+main().then(() => info("Complete!"))
 
 async function main() {
     try {
-        const manifestPath = core.getInput("manifest");
-        const extractPath = core.getInput("path");
-        const rawAliases = core.getInput("aliases");
+        const manifestPath = getInput("manifest");
+        const extractPath = getInput("path");
+        const rawAliases = getInput("aliases");
 
         const depAliases = JSON.parse(rawAliases);
 
         if (depAliases != {}) {
             const aliasKeys = Object.keys(depAliases);
             aliasKeys.forEach(key => {
-                core.info(`Given alias '${key}': '${depAliases[key]}'`);
+                info(`Given alias '${key}': '${depAliases[key]}'`);
             });
         }
 
-        let manifestStringData = fs.readFileSync(manifestPath, 'utf8');
+        let manifestStringData = readFileSync(manifestPath, 'utf8');
         if (manifestStringData.startsWith('\uFEFF')) {
-            core.warning("BOM character detected at the beginning of the manifest JSON file. Please remove the BOM from the file as it does not conform to the JSON spec (https://datatracker.ietf.org/doc/html/rfc7159#section-8.1) and may cause issues regarding interoperability.")
+            warning("BOM character detected at the beginning of the manifest JSON file. Please remove the BOM from the file as it does not conform to the JSON spec (https://datatracker.ietf.org/doc/html/rfc7159#section-8.1) and may cause issues regarding interoperability.")
             manifestStringData = manifestStringData.slice(1);
         }
 
         const manifest = JSON.parse(manifestStringData);
-        core.info("Retrieved manifest of '" + manifest.id + "' version '" + manifest.version + "'");
+        info("Retrieved manifest of '" + manifest.id + "' version '" + manifest.version + "'");
 
         const gameVersions = await fetchJson("https://versions.beatmods.com/versions.json");
         const versionAliases = await fetchJson("https://alias.beatmods.com/aliases.json");
@@ -38,23 +38,23 @@ async function main() {
             throw new Error("Game version '" + manifest.gameVersion + "' doesn't exist.");
         }
 
-        core.info("Fetching mods for game version '" + version + "'");
+        info("Fetching mods for game version '" + version + "'");
         const mods = await fetchJson("https://beatmods.com/api/v1/mod?gameVersion=" + version);
 
         for (let depName of Object.keys(manifest.dependsOn)) {
             const depVersion = manifest.dependsOn[depName];
-            const dependency = mods.find(x => (x.name === depName || x.name == depAliases[depName]) && semver.satisfies(x.version, depVersion));
+            const dependency = mods.find(x => (x.name === depName || x.name == depAliases[depName]) && satisfies(x.version, depVersion));
 
             if (dependency != null) {
                 const depDownload = dependency.downloads.find(x => x.type === "universal").url;
-                core.info("Downloading mod '" + depName + "' version '" + dependency.version + "'");
+                info("Downloading mod '" + depName + "' version '" + dependency.version + "'");
                 await download("https://beatmods.com" + depDownload, extractPath);
             } else {
-                core.warning("Mod '" + depName + "' version '" + depVersion + "' not found.");
+                warning("Mod '" + depName + "' version '" + depVersion + "' not found.");
             }
         }
     } catch (error) {
-        core.setFailed(error.message);
+        setFailed(error.message);
     }
 }
 
@@ -65,5 +65,5 @@ async function fetchJson(url) {
 
 async function download(url, extractPath) {
     const response = await fetch(url);
-    await response.body.pipe(unzip.Extract({path: extractPath}));
+    await response.body.pipe(Extract({path: extractPath}));
 }
