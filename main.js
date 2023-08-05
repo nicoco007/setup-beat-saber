60a915453ab35a0d3d1ee87e37f894858fccbeed
Nicolas Gnyra <nicolas@gnyra.com>
2023-08-04 21:59:03 -0400

    Add tests

--- main.js
+++ main.js
@@ -2,68 +2,67 @@ import { info, getInput, warning, setFailed } from '@actions/core';
 import fetch from 'node-fetch';
 import { satisfies } from 'semver';
 import { Extract } from 'unzipper';
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 import { copySync } from "fs-extra/esm";
 import { join } from "path";
+import { env } from "process";
 
-main().then(() => info("Complete!"))
+if (env["NODE_ENV"] != "test") {
+    main()
+        .catch((error) => setFailed(error.message))
+        .then(() => info("Complete!"));
+}
 
-async function main() {
-    try {
-        const manifestPath = getInput("manifest");
-        const extractPath = getInput("path");
-        const rawAliases = getInput("aliases");
+export async function main() {
+    const manifestPath = getInput("manifest");
+    const extractPath = getInput("path");
+    const rawAliases = getInput("aliases");
 
-        const depAliases = JSON.parse(rawAliases);
+    const depAliases = JSON.parse(rawAliases || null) || {};
 
-        if (depAliases != {}) {
-            const aliasKeys = Object.keys(depAliases);
-            aliasKeys.forEach(key => {
-                info(`Given alias '${key}': '${depAliases[key]}'`);
-            });
-        }
+    if (depAliases != {}) {
+        Object.entries(depAliases).forEach(([key, value]) => {
+            info(`Given alias '${key}': '${value}'`);
+        });
+    }
 
-        let manifestStringData = readFileSync(manifestPath, 'utf8');
-        if (manifestStringData.startsWith('\uFEFF')) {
-            warning("BOM character detected at the beginning of the manifest JSON file. Please remove the BOM from the file as it does not conform to the JSON spec (https://datatracker.ietf.org/doc/html/rfc7159#section-8.1) and may cause issues regarding interoperability.")
-            manifestStringData = manifestStringData.slice(1);
-        }
+    let manifestStringData = readFileSync(manifestPath, 'utf8');
+    if (manifestStringData.startsWith('\uFEFF')) {
+        warning("BOM character detected at the beginning of the manifest JSON file. Please remove the BOM from the file as it does not conform to the JSON spec (https://datatracker.ietf.org/doc/html/rfc7159#section-8.1) and may cause issues regarding interoperability.")
+        manifestStringData = manifestStringData.slice(1);
+    }
 
-        const manifest = JSON.parse(manifestStringData);
-        info("Retrieved manifest of '" + manifest.id + "' version '" + manifest.version + "'");
+    const manifest = JSON.parse(manifestStringData);
+    info("Retrieved manifest of '" + manifest.id + "' version '" + manifest.version + "'");
 
-        const wantedGameVersion = getInput("game-version", { required: false }) || manifest.gameVersion;
-        const gameVersions = await fetchJson("https://versions.beatmods.com/versions.json");
-        const versionAliases = await fetchJson("https://alias.beatmods.com/aliases.json");
+    const wantedGameVersion = getInput("game-version", { required: false }) || manifest.gameVersion;
+    const gameVersions = await fetchJson("https://versions.beatmods.com/versions.json");
+    const versionAliases = await fetchJson("https://alias.beatmods.com/aliases.json");
 
-        const version = gameVersions.find(x => x === wantedGameVersion || versionAliases[x].some(y => y === wantedGameVersion));
-        if (version == null) {
-            throw new Error("Game version '" + wantedGameVersion + "' doesn't exist.");
-        }
+    const version = gameVersions.find(x => x === wantedGameVersion || versionAliases[x].some(y => y === wantedGameVersion));
+    if (version == null) {
+        throw new Error("Game version '" + wantedGameVersion + "' doesn't exist.");
+    }
 
-        info("Fetching mods for game version '" + version + "'");
-        const mods = await fetchJson("https://beatmods.com/api/v1/mod?sort=version&sortDirection=-1&gameVersion=" + version);
+    info("Fetching mods for game version '" + version + "'");
+    const mods = await fetchJson("https://beatmods.com/api/v1/mod?sort=version&sortDirection=-1&gameVersion=" + version);
 
-        for (let depName of Object.keys(manifest.dependsOn)) {
-            const depVersion = manifest.dependsOn[depName];
-            const dependency = mods.find(x => (x.name === depName || x.name == depAliases[depName]) && satisfies(x.version, depVersion));
+    for (const [depName, depVersion] of Object.entries(manifest.dependsOn)) {
+        const dependency = mods.find(x => (x.name === depName || x.name == depAliases[depName]) && satisfies(x.version, depVersion));
 
-            if (dependency != null) {
-                const depDownload = dependency.downloads.find(x => x.type === "universal").url;
-                info("Downloading mod '" + depName + "' version '" + dependency.version + "'");
-                await download("https://beatmods.com" + depDownload, extractPath);
+        if (dependency != null) {
+            const depDownload = dependency.downloads.find(x => x.type === "universal").url;
+            info("Downloading mod '" + depName + "' version '" + dependency.version + "'");
+            await download("https://beatmods.com" + depDownload, extractPath);
 
-                // special case since BSIPA moves files at runtime
-                if (depName === "BSIPA") {
-                    copySync(join(extractPath, "IPA", "Libs"), join(extractPath, "Libs"), { overwrite: true });
-                    copySync(join(extractPath, "IPA", "Data"), join(extractPath, "Beat Saber_Data"), { overwrite: true });
-                }
-            } else {
-                warning("Mod '" + depName + "' version '" + depVersion + "' not found.");
+            // special case since BSIPA moves files at runtime
+            if (depName === "BSIPA") {
+                copySync(join(extractPath, "IPA", "Libs"), join(extractPath, "Libs"), { overwrite: true });
+                copySync(join(extractPath, "IPA", "Data"), join(extractPath, "Beat Saber_Data"), { overwrite: true });
             }
+        } else {
+            warning("Mod '" + depName + "' version '" + depVersion + "' not found.");
         }
-    } catch (error) {
-        setFailed(error.message);
     }
 }
 
@@ -76,7 +75,7 @@ async function download(url, extractPath) {
     const response = await fetch(url);
     const stream = Extract({path: extractPath});
     const promise = new Promise((resolve) => {
-        stream.on('finish', () => {
+        stream.on('close', () => {
             resolve();
         });
     });
---
+++ main.test.js
@@ -0,0 +1,129 @@
+import { jest } from "@jest/globals";
+import { fileURLToPath } from "url";
+import * as process from "process";
+import * as path from "path";
+import sinon from "sinon";
+import * as nf from "node-fetch";
+import { readFileSync, createReadStream, rmSync } from "fs";
+import * as ac from "@actions/core";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+jest.unstable_mockModule("node-fetch", () => ({
+    ...nf,
+    __esModule: true,
+    default: sinon.stub(),
+}));
+
+jest.unstable_mockModule("@actions/core", async () => ({
+    ...ac,
+    __esModule: true,
+    info: jest.fn(),
+    warning: jest.fn(),
+}))
+
+const { default: fetch } = await import("node-fetch");
+const { main } = await import("./main");
+const core = await import ("@actions/core");
+
+function mockFetch(url, body, status = 200) {
+    fetch.withArgs(url).returns(new nf.Response(body, {
+        status: status,
+        headers: new nf.Headers({
+            "Content-Type": "application/json",
+        }),
+    }));
+}
+
+describe("main", () => {
+    beforeEach(() => {
+        fetch.withArgs(sinon.match(/https:\/\/beatmods.com\/uploads\/.*/)).callsFake(() =>
+            new nf.Response(
+                createReadStream(path.join(__dirname, "tests", "dummy.zip")),
+                {
+                    status: 200,
+                    headers: new nf.Headers({ "Content-Type": "application/octet-stream" })
+                }
+            )
+        );
+
+        mockFetch("https://versions.beatmods.com/versions.json", JSON.stringify(["1.13.2", "1.16.1"]));
+        mockFetch("https://alias.beatmods.com/aliases.json", JSON.stringify({ "1.13.2": [], "1.16.1": ["1.16.2"] }));
+        mockFetch("https://beatmods.com/api/v1/mod?sort=version&sortDirection=-1&gameVersion=1.13.2", readFileSync(path.join(__dirname, "tests", "mods_1.13.2.json")));
+        mockFetch("https://beatmods.com/api/v1/mod?sort=version&sortDirection=-1&gameVersion=1.16.1", readFileSync(path.join(__dirname, "tests", "mods_1.16.1.json")));
+    });
+
+    test("downloads all mods listed in manifest", async () => {
+        process.env["INPUT_MANIFEST"] = path.join(__dirname, "tests", "basic.json");
+        process.env["INPUT_PATH"] = path.join(__dirname, "Refs");
+
+        await main();
+
+        expect(core.info).toHaveBeenCalledWith("Retrieved manifest of 'examplemod' version '0.1.0'");
+        expect(core.info).toHaveBeenCalledWith("Fetching mods for game version '1.13.2'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'BSIPA' version '4.1.4'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'BS Utils' version '1.7.0'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'SongCore' version '3.1.0'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'BeatSaverSharp' version '2.0.0'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'BeatSaberMarkupLanguage' version '1.4.5'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'SiraUtil' version '2.5.1'");
+    });
+
+    test("resolves version aliases", async () => {
+        process.env["INPUT_MANIFEST"] = path.join(__dirname, "tests", "version_alias.json");
+        process.env["INPUT_PATH"] = path.join(__dirname, "Refs");
+
+        await main();
+
+        expect(core.info).toHaveBeenCalledWith("Retrieved manifest of 'examplemod' version '0.1.0'");
+        expect(core.info).toHaveBeenCalledWith("Fetching mods for game version '1.16.1'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'BSIPA' version '4.1.6'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'SongCore' version '3.5.0'");
+    });
+
+    test("resolves mod aliases", async () => {
+        process.env["INPUT_MANIFEST"] = path.join(__dirname, "tests", "mod_alias.json");
+        process.env["INPUT_PATH"] = path.join(__dirname, "Refs");
+        process.env["INPUT_ALIASES"] = JSON.stringify({ "CustomAvatar": "Custom Avatars" });
+
+        await main();
+
+        expect(core.info).toHaveBeenCalledWith("Given alias 'CustomAvatar': 'Custom Avatars'");
+        expect(core.info).toHaveBeenCalledWith("Retrieved manifest of 'examplemod' version '0.1.0'");
+        expect(core.info).toHaveBeenCalledWith("Fetching mods for game version '1.13.2'");
+        expect(core.info).toHaveBeenCalledWith("Downloading mod 'CustomAvatar' version '5.1.2'");
+    });
+
+    test("warns for missing mods", async () => {
+        process.env["INPUT_MANIFEST"] = path.join(__dirname, "tests", "nonexistent_mod.json");
+        process.env["INPUT_PATH"] = path.join(__dirname, "Refs");
+
+        await main();
+
+        expect(core.warning).toHaveBeenCalledWith("Mod 'MissingMod' version '^1.0.0' not found.");
+    });
+
+    test("warns for missing versions", async () => {
+        process.env["INPUT_MANIFEST"] = path.join(__dirname, "tests", "nonexistent_version.json");
+        process.env["INPUT_PATH"] = path.join(__dirname, "Refs");
+
+        await main();
+
+        expect(core.warning).toHaveBeenCalledWith("Mod 'BeatSaverSharp' version '^2000.0.0' not found.");
+    });
+
+    test("warns if manifest has a BOM", async () => {
+        process.env["INPUT_MANIFEST"] = path.join(__dirname, "tests", "with_bom.json");
+        process.env["INPUT_PATH"] = path.join(__dirname, "Refs");
+
+        await main();
+
+        expect(core.warning).toHaveBeenCalledWith("BOM character detected at the beginning of the manifest JSON file. Please remove the BOM from the file as it does not conform to the JSON spec (https://datatracker.ietf.org/doc/html/rfc7159#section-8.1) and may cause issues regarding interoperability.");
+    });
+
+    afterEach(() => {
+        rmSync(path.join(__dirname, "Refs"), { recursive: true, force: true });
+        sinon.restore();
+        jest.resetAllMocks();
+    })
+});
